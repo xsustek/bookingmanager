01f736f94356b6a880c958d984283ba8d4e06465
Tomas Kopecky <357217@mail.muni.cz>
2017-10-26 17:52:42 +0200

    UserDao implemented.

--- bookingmanagerpersistence/src/main/java/cz/fi/muni/pa165/dao/UserDao.java
+++ bookingmanagerpersistence/src/main/java/cz/fi/muni/pa165/dao/UserDao.java
@@ -1,4 +1,18 @@
 package cz.fi.muni.pa165.dao;
 
+import cz.fi.muni.pa165.entity.User;
+
+import java.util.List;
+
 public interface UserDao {
+
+    void create(User user);
+
+    User findById(long id);
+
+    void update(User user);
+
+    void remove(User user);
+
+    List<User> findAll();
 }
--- bookingmanagerpersistence/src/main/java/cz/fi/muni/pa165/dao/UserDaoImpl.java
+++ bookingmanagerpersistence/src/main/java/cz/fi/muni/pa165/dao/UserDaoImpl.java
@@ -1,4 +1,40 @@
 package cz.fi.muni.pa165.dao;
 
-public class UserDaoImpl {
+import cz.fi.muni.pa165.entity.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void create(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public User findById(long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void update(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public void remove(User user) {
+        entityManager.remove(entityManager.merge(user));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return entityManager.createQuery("select u from User u", User.class).getResultList();
+    }
 }
