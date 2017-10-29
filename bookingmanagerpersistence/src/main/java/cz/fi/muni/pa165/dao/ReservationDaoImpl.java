bf4a2ffb32f883cd3908ab38ef294c657207e830
Viktoria Tibenska <vtibensk8@gmail.com>
2017-10-29 13:48:12 +0100

    added javadoc for reservation and reservationDao

--- bookingmanagerpersistence/src/main/java/cz/fi/muni/pa165/dao/ReservationDaoImpl.java
+++ bookingmanagerpersistence/src/main/java/cz/fi/muni/pa165/dao/ReservationDaoImpl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.fi.muni.pa165.dao;
 
 import cz.fi.muni.pa165.entity.Reservation;
@@ -24,7 +19,7 @@ public class ReservationDaoImpl implements ReservationDao {
     }
 
     @Override
-    public Reservation findById(long id) {
+    public Reservation findById(Long id) {
         return entityManager.find(Reservation.class, id);
     }
 
--- bookingmanagerpersistence/src/main/java/cz/fi/muni/pa165/entity/Reservation.java
+++ bookingmanagerpersistence/src/main/java/cz/fi/muni/pa165/entity/Reservation.java
@@ -5,6 +5,8 @@ import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 /**
+ * Reservation class represents a booking user can make to book a hotel room.
+ * 
  * @author Viktoria Tibenska
  */
 @Entity
@@ -31,42 +33,92 @@ public class Reservation {
     public Reservation() {
     }
 
+    /**
+     * Returns id of a reservation
+     * 
+     * @return id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets reservation id to a new, specified value
+     * 
+     * @param id New value for id
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Returns user associated with reservation
+     * 
+     * @return User
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Sets user associated with reservation to a new value
+     * 
+     * @param user New user for reservation
+     */
     public void setUser(User user) {
         this.user = user;
     }
 
+    /**
+     * Gets room associated with reservation
+     * 
+     * @return Room
+     */
     public Room getRoom() {
         return room;
     }
 
+    /**
+     * Sets room associated with reservation to a new value
+     * 
+     * @param room New room for reservation
+     */
     public void setRoom(Room room) {
         this.room = room;
     }
 
+    /**
+     * Gets start time of a reservation
+     * 
+     * @return Start time of a reservation
+     */
     public LocalDateTime getStartTime() {
         return startTime;
     }
 
+    /**
+     * Sets start time of a reservation
+     * 
+     * @param startTime New start time value
+     */
     public void setStartTime(LocalDateTime startTime) {
         this.startTime = startTime;
     }
 
+    /**
+     * Gets end time of a reservation
+     * 
+     * @return End time of a reservation
+     */
     public LocalDateTime getEndTime() {
         return endTime;
     }
 
+    /**
+     * Sets end time of a reservation
+     * 
+     * @param endTime New end time value
+     */
     public void setEndTime(LocalDateTime endTime) {
         this.endTime = endTime;
     }
