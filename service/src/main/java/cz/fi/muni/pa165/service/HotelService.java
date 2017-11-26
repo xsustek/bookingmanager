163cf3b920cea25b0d2b603e28f9a7b92c4cb75a
Milan Šůstek <milan.sustek@gmail.com>
2017-11-26 16:29:59 +0100

    HotelService interface

--- service/src/main/java/cz/fi/muni/pa165/service/HotelService.java
+++ service/src/main/java/cz/fi/muni/pa165/service/HotelService.java
@@ -1,4 +1,22 @@
 package cz.fi.muni.pa165.service;
 
+import cz.fi.muni.pa165.entity.Hotel;
+import cz.fi.muni.pa165.entity.Room;
+
+import java.util.List;
+
 public interface HotelService {
+    void create(Hotel hotel);
+
+    Hotel findById(long id);
+
+    List<Hotel> findByName(String name);
+
+    void update(Hotel hotel);
+
+    void delete(Hotel hotel);
+
+    void addRoom(Hotel hotel, Room room);
+
+    List<Hotel> findAll();
 }
