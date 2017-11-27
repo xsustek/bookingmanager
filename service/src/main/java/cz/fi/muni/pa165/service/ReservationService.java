a90921935c133fcbd23dbbf20d74fe5f5ef52270
Viktoria Tibenska <vtibensk8@gmail.com>
2017-11-27 05:52:32 +0100

    ReservationService interface and its implementation

---
+++ service/src/main/java/cz/fi/muni/pa165/service/ReservationService.java
@@ -0,0 +1,66 @@
+package cz.fi.muni.pa165.service;
+
+import cz.fi.muni.pa165.entity.Reservation;
+import cz.fi.muni.pa165.entity.Room;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author Viktoria Tibenska
+ */
+public interface ReservationService {
+    /**
+     * Method to add a new reservation.
+     *
+     * @param reservation new booking to add
+     */
+    void createReservation(Reservation reservation);
+
+    /**
+     * Method to delete a specific reservation.
+     *
+     * @param reservation specific reservation to be deleted
+     */
+    void removeReservation(Reservation reservation);
+
+    /**
+     * Method to update a specific reservation meaning update its parameters.
+     *
+     * @param reservation reservation to be updated
+     */
+    void updateReservation(Reservation reservation);
+
+    /**
+     * Method to get desired reservation by its ID.
+     *
+     * @param id id of desired reservation
+     * @return reservation with a matching ID
+     */
+    Reservation getReservationById(Long id);
+
+    /**
+     * Method to get all reservations.
+     *
+     * @return a collection of all reservations
+     */
+    List<Reservation> getAllReservations();
+    
+    /**
+     * Method to get the reservations in specified interval
+     * 
+     * @param fromTime the beginning of the interval
+     * @param tillTime the end of the interval
+     * 
+     * @return List of all reservation which are active in the interval defined by parameters
+     */
+    List<Reservation> getReservationsByInterval(LocalDateTime fromTime, LocalDateTime tillTime);
+    
+    /**
+     * Method to get all reservations for specific room
+     * 
+     * @param room Room of a hotel to filter the reservations by
+     * 
+     * @return List of all reservations of a room
+     */
+    List<Reservation> getReservationsByRoom(Room room);
+}
---
+++ service/src/main/java/cz/fi/muni/pa165/service/ReservationServiceImpl.java
@@ -0,0 +1,75 @@
+package cz.fi.muni.pa165.service;
+
+import cz.fi.muni.pa165.dao.ReservationDao;
+import cz.fi.muni.pa165.entity.Reservation;
+import cz.fi.muni.pa165.entity.Room;
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.inject.Inject;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Viktoria Tibenska
+ */
+@Service
+public class ReservationServiceImpl implements ReservationService {
+    
+    @Inject ReservationDao reservationDao;
+
+    @Override
+    public void createReservation(Reservation reservation) {
+        
+        boolean error = false;
+        List<Reservation> reservations = reservationDao.getReservationsByRoom(reservation.getRoom());
+        
+        // Checking if there is no overlaping reservation for specified room in the requested dates
+        for (Reservation r: reservations) {
+            LocalDateTime existingStart = r.getStartTime();
+            LocalDateTime existingEnd = r.getEndTime();
+            LocalDateTime newStart = reservation.getStartTime();
+            LocalDateTime newEnd = reservation.getEndTime();
+            
+            if (!((existingStart.isAfter(newEnd) && existingEnd.isAfter(newEnd)) 
+               || (existingStart.isBefore(newStart) && existingEnd.isBefore(newStart)))) {
+                error = true;
+            }
+        }
+        
+        if (error) {
+            throw new IllegalArgumentException("The room is already booked during specified dates!");
+        } else {
+            reservationDao.create(reservation);
+        }
+    }
+
+    @Override
+    public void removeReservation(Reservation reservation) {
+        reservationDao.remove(reservation);
+    }
+
+    @Override
+    public void updateReservation(Reservation reservation) {
+        reservationDao.update(reservation);
+    }
+
+    @Override
+    public Reservation getReservationById(Long id) {
+        return reservationDao.findById(id);
+    }
+
+    @Override
+    public List<Reservation> getAllReservations() {
+        return reservationDao.findAll();
+    }
+
+    @Override
+    public List<Reservation> getReservationsByInterval(LocalDateTime fromTime, LocalDateTime tillTime) {
+        return reservationDao.getReservationsByInterval(fromTime, tillTime);
+    }
+
+    @Override
+    public List<Reservation> getReservationsByRoom(Room room) {
+        return reservationDao.getReservationsByRoom(room);
+    }
+    
+}
