951b9aa9616b3941a214b9b49079c428e9aae5b4
Peter Neupauer <peter.neupauer@avito.cz>
2017-11-27 17:51:20 +0100

    Added RoomService Tests

---
+++ service/src/test/java/cz/fi/muni/pa165/RoomBuilder.java
@@ -0,0 +1,61 @@
+package cz.fi.muni.pa165;
+
+import cz.fi.muni.pa165.entity.Hotel;
+import cz.fi.muni.pa165.entity.Reservation;
+import cz.fi.muni.pa165.entity.Room;
+import cz.fi.muni.pa165.enums.RoomType;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RoomBuilder {
+
+    private long id;
+    private BigDecimal price;
+    private RoomType type;
+    private int capacity;
+    private Set<Reservation> reservations = new HashSet<>();
+    private Hotel hotel;
+
+    public RoomBuilder id(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public RoomBuilder price(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public RoomBuilder type(RoomType type) {
+        this.type = type;
+        return this;
+    }
+
+    public RoomBuilder capacity(int capacity) {
+        this.capacity = capacity;
+        return this;
+    }
+
+    public RoomBuilder reservations(Set<Reservation> reservations) {
+        this.reservations = reservations;
+        return this;
+    }
+
+    public RoomBuilder hotel(Hotel hotel) {
+        this.hotel = hotel;
+        return this;
+    }
+
+    public Room build() {
+        Room room = new Room();
+        room.setId(this.id);
+        room.setPrice(this.price);
+        room.setType(this.type);
+        room.setCapacity(this.capacity);
+        room.setReservations(this.reservations);
+        room.setHotel(this.hotel);
+        return room;
+    }
+}
--- service/src/test/java/cz/fi/muni/pa165/RoomServiceTest.java
+++ service/src/test/java/cz/fi/muni/pa165/RoomServiceTest.java
@@ -1,4 +1,134 @@
 package cz.fi.muni.pa165;
 
+import cz.fi.muni.pa165.dao.RoomDao;
+import cz.fi.muni.pa165.entity.Hotel;
+import cz.fi.muni.pa165.entity.Reservation;
+import cz.fi.muni.pa165.entity.Room;
+import cz.fi.muni.pa165.enums.RoomType;
+import cz.fi.muni.pa165.service.RoomService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.List;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ContextConfiguration(classes = ServiceApplicationContext.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
 public class RoomServiceTest {
+
+    @Inject
+    private RoomService roomService;
+
+    @Inject
+    private RoomDao repository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test
+    public void findAllRooms() {
+        Room kingRoom = getTestRoom().build();
+        Room singleRoom = getTestRoom().type(RoomType.SINGLE).build();
+
+        repository.create(kingRoom);
+        repository.create(singleRoom);
+
+        List<Room> rooms = roomService.findAll();
+
+        assertThat(rooms).isNotEmpty().containsExactly(kingRoom, singleRoom);
+    }
+
+    @Test
+    public void findRoomsByType() {
+        Room kingRoom = getTestRoom().build();
+        Room singleRoom = getTestRoom().type(RoomType.SINGLE).build();
+
+        repository.create(kingRoom);
+        repository.create(singleRoom);
+
+        List<Room> rooms = roomService.findByType(RoomType.KING);
+
+        assertThat(rooms).isNotEmpty().containsExactly(kingRoom);
+    }
+
+    @Test
+    public void findRoomsByHotel() {
+        Hotel h1 = new Hotel();
+        Hotel h2 = new Hotel();
+
+        Room kingRoom = getTestRoom().hotel(h1).build();
+        Room singleRoom = getTestRoom().type(RoomType.SINGLE).hotel(h2).build();
+
+        repository.create(kingRoom);
+        repository.create(singleRoom);
+
+        List<Room> rooms = roomService.findByHotel(h1);
+
+        assertThat(rooms).isNotEmpty().containsExactly(kingRoom);
+    }
+
+    @Test
+    public void create() {
+        Room expectedRoom = getTestRoom().build();
+
+        roomService.create(expectedRoom);
+
+        Room room = repository.findById(expectedRoom.getId());
+
+        assertThat(room).isNotNull().isEqualTo(room);
+    }
+
+    @Test
+    public void update() {
+        Room expectedRoom = getTestRoom().build();
+
+        repository.create(expectedRoom);
+
+        Room room = repository.findById(expectedRoom.getId());
+
+        assertThat(room).isNotNull().isEqualTo(room);
+
+        expectedRoom.setCapacity(1);
+
+        roomService.update(expectedRoom);
+
+        room = repository.findById(expectedRoom.getId());
+
+        assertThat(room).isNotNull().isEqualTo(room);
+    }
+
+    @Test
+    public void remove() {
+        Room kingRoom = getTestRoom().build();
+        Room singleRoom = getTestRoom().type(RoomType.SINGLE).build();
+
+        repository.create(kingRoom);
+        repository.create(singleRoom);
+
+        roomService.remove(singleRoom);
+
+        List<Room> rooms = roomService.findByType(RoomType.KING);
+
+        assertThat(rooms).isNotEmpty().containsExactly(kingRoom);
+    }
+
+
+    private RoomBuilder getTestRoom() {
+        return new RoomBuilder()
+                .capacity(10)
+                .price(BigDecimal.valueOf(5000L))
+                .type(RoomType.KING)
+                .reservations(new HashSet<Reservation>());
+    }
 }
