29064d7df9f3675710a0e757035e2fc8113905d6
Peter Neupauer <peter.neupauer@avito.cz>
2017-12-17 20:28:45 +0100

    React router setup

---
+++ frontend/react-src/js/screens/AdminScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class AdminScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Admin</h2>
+            </div>
+        );
+    }
+}
